16ff29b17480abc087877069f58565a43fa7c501
Suhendarprogrammer <45907948+Suhendarprogrammer@users.noreply.github.com>
2018-12-18 13:16:51 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int s,v,t;
+    cout<<"********PROGRAM MENGHITUNG JARAK TEMPUH*********\n";
+    cout<<"=================================================\n";
+    cout<<"\nMasukkan Kecepatan Yang Ingin Ditempuh : ";
+    cin>>v;
+    cout<<"\nMasukkan Waktu Yang Ingin Ditempuh : ";
+    cin>>t;
+    s=v*t;
+    cout<<"Maka,Jarak Yang Ditempuh Adalah : "<<s<<"\n";
+    return 0;
+}
